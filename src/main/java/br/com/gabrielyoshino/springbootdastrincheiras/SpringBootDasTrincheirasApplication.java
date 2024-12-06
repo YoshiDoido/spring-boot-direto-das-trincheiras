65e49c30b7a4f410c3c738d38ee1a0c15f0a431d
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-12-06 01:00:44 -0300

    Projeto criado

---
+++ src/main/java/br/com/gabrielyoshino/springbootdastrincheiras/SpringBootDasTrincheirasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielyoshino.springbootdastrincheiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDasTrincheirasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDasTrincheirasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabrielyoshino/springbootdastrincheiras/SpringBootDasTrincheirasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielyoshino.springbootdastrincheiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDasTrincheirasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
